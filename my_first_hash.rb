dfd8b5da83d1027567bc5bb7b42af4e4fb396e0a
Evelyn Kosma <ezranaja@gmail.com>
2021-03-01 00:04:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                  }
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  shipping_manifest = adding
+  return shipping_manifest
 end
 
 # add 2 muskets to the shipping_manifest hash below
