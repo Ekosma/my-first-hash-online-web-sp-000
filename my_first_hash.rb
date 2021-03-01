caeceb950798d04639cb55937bb50a82888142a4
Evelyn Kosma <ezranaja@gmail.com>
2021-03-01 00:04:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                  }
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  shipping_manifest[]
+  shipping_manifest = adding
 end
 
 # add 2 muskets to the shipping_manifest hash below
